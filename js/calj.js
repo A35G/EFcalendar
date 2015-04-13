6c0f03170a9b719199d8c36ed9b35e94cd6f92b5
A35G <a35g@hackworld.it>
2015-04-14 01:09:16 +0200

    Init Project

---
+++ js/calj.js
@@ -0,0 +1,21 @@
+var EFcalendar = function () {
+
+	var sayHello = function () {
+
+		alert('Test!');
+
+	}
+
+	return {
+
+		init: function() {
+
+			sayHello();
+
+		}
+
+	}
+
+}();
+
+EFcalendar.init();
